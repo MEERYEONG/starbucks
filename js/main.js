4c3edbf3e5e1efc72056c75dc05f315c1350d63d
MEERYEONG <dmb00@naver.com>
2022-09-16 21:25:58 +0900

    공통 모듈 분리

--- js/main.js
+++ js/main.js
@@ -1,21 +1,3 @@
-const searchEL = document.querySelector('.search');
-const searchInputEL = searchEL.querySelector('input');
-
-searchEL.addEventListener('click',function(){
-   searchInputEL.focus();
-});
-
-searchInputEL.addEventListener('focus',function(){
-  searchEL.classList.add('focused');
-  searchInputEL.setAttribute('placeholder','통합검색');
-});
-
-searchInputEL.addEventListener('blur',function(){
-  searchEL.classList.remove('focused');
-  searchInputEL.setAttribute('placeholder','');
-});
-
-
 const badgeEL = document .querySelector('header .badges');
 const toTopEl = document.querySelector('#to-top');
 
@@ -150,6 +132,4 @@ new ScrollMagic
 });
 
 
-//변수/변수이름/   객체    /메소드를 실행해서/ 요소를 찾아서 할당할것임. 요소가 .this-year//
-const thisYear = document.querySelector('.this-year');
-thisYear.textContent = new Date().getFullYear(); // 2021
+
